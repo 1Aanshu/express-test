faeda56ca4767aa1202ca8bc0ec6e2460180bf0f
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-29 22:51:19 +0545

    Use of callback function

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const a = 5;
-const b = 9;
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
-console.log(a + b);
+app.get("/", (req, res)=> {
+  res.json({ msg: "Hello world" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}.Url is http://localhost:${PORT}`);
+});
+
+// GET => fetch data from server to client
+// POST => create new data (client to server)
+// PUT => Update more than 1 data of existing data (client to server)
+// PATCH => update 1 data only
+// DELETE => remove existing data (server to client)
