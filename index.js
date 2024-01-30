73032aa4f217410b94822c7ac9b76c9642afb167
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-30 09:29:17 +0545

    Starting of API

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+app.use(express.json());
 
-app.get("/", (req, res)=> {
-  res.json({ msg: "Hello world" });
+// (READ)
+app.get("/", (req, res) => {
+  // logic => db
+  res.json({ msg: "all blogs list" });
+});
+
+// (CREATE)
+app.post("/", (req, res) => {
+  const data = req.body;
+  // logic => send this data to db
+  res.json({ msg: `New blog added with data: ${data}` });
+});
+
+// (UPDATE)
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  // logic => send this data to db
+  res.json({ msg: `Blog Id ${id} put with data: ${data}` });
+});
+
+// (UPDATE)
+app.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  // logic => send this data to db
+  res.json({ msg: `Blog Id ${id} patched with data: ${data}` });
+});
+
+//(DELETE)
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ msg: `Blog Id ${id} deleted` });
 });
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}.Url is http://localhost:${PORT}`);
 });
+
 
 // GET => fetch data from server to client
 // POST => create new data (client to server)
